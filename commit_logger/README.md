788234e9eb41a819d2cd784ed55fb06ece46c9ad
Janet Liang <janet@harmony.one>
2020-04-10 18:23:24 -0700

    [commit_logger] Require specific configuration file & command to generate sample [commit_logger] Allow usage for multiple networks at the same time [commit_logger] Update README

--- commit_logger/README.md
+++ commit_logger/README.md
@@ -1,21 +1,37 @@
 ### Commit Logger
 
+Track Network version deployed & longevity
+
 #### Requirements
 `python3 -m pip install -r requirements.txt`
 
-#### Run commands
-Using a Tmux session, run the logger:
+#### Setup
+To generate sample input file `network_information.txt` in the root directory, run:
 ```
-python3 commit_logger.py --endpoints [https://api.s0.b.hmny.io,...]
+python3 commit_logger.py --setup
 ```
-Split into 2 panes & run the flask server:
+Do not change the name of the file or move the file.
+Sample is generated with Mainnet as the example.
+
+Warning: Having too many request targets in the config_file might cause "too many files" errors for the requests.
+
+#### Run data collection
+```
+python3 commit_logger.py
+```
+
+#### Run flask server for JSON webpage
 ```
 export FLASK_APP=commit_log
-python3 -m flask run -p [port]
+python3 -m flask run
 ```
+
+Give the `-p [PORT]` parameter to specify the port for the server.
 
 #### Output
 ```
 ./data/commit.json
 ./data/commit_*.log
 ```
+
+Can also see the JSON output displayed on http://localhost:5000/commit_log (Replace 5000 with the port specified if different)
--- commit_logger/commit_logger.py
+++ commit_logger/commit_logger.py
@@ -18,6 +18,7 @@ backup_log = path.join(data, 'commit.json.bk')
 file_time_fmt = '%m%d_%H%I%S'
 read_time_fmt = '%m/%d_%H:%I:%S'
 headers = {'Content-Type': 'application/json'}
+config_file = path.abspath(path.join(base, 'network_information.txt'))
 
 def latestBlock() -> dict:
     return {"id": "1", "jsonrpc": "2.0",
@@ -43,15 +44,37 @@ if __name__ == '__main__':
     formatted_time = datetime.now().strftime(file_time_fmt)
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--endpoints', help = 'Endpoints to query from, sorted by shard & seperated by commas')
-    parser.add_argument('--output_file', default = 'commit_%s.log' % formatted_time, help = 'File to output log to')
+    parser.add_argument('--setup', action = 'store_true', help = 'Generate sample config file')
     parser.add_argument('--sleep', default = 60, type = int, help = 'Sleep timer')
+    parser.add_argument('--verbose', action = 'store_true', help = 'Verbose for debug')
 
     args = parser.parse_args()
 
-    endpoint = []
-    if args.endpoints:
-        endpoint = [x.strip() for x in args.endpoints.strip().split(',')]
+    if args.setup:
+        if not path.exists(config_file):
+            with open(config_file, 'w') as f:
+                f.write('mainnet, https://api.s0.t.hmny.io\n')
+                f.write('mainnet, https://api.s1.t.hmny.io\n')
+                f.write('mainnet, https://api.s2.t.hmny.io\n')
+                f.write('mainnet, https://api.s3.t.hmny.io')
+            print('Sample config file: %s' % config_file)
+        else:
+            print('Config file already exists: %s' % config_file)
+        exit()
+
+    if not path.exists(config_file):
+        print('[ERROR] Missing config file: %s' % config_file)
+        print('Use --setup to generate a sample file.')
+        exit()
+
+    networks = defaultdict(list)
+    with open(config_file, 'r') as f:
+        for l in f:
+            try:
+                net, endpoint = tuple(x.strip() for x in l.strip().split(','))
+            except:
+                print('[ERROR] Config file format does not match required format [network, endpoint]: %s' % l)
+            networks[net].append(endpoint)
 
     if not path.exists(data):
         try:
@@ -63,14 +86,14 @@ if __name__ == '__main__':
     # Set up logger
     logger = logging.getLogger("commit_logger")
     logger.setLevel(logging.INFO)
-    file_handler = logging.FileHandler("data/%s" % args.output_file)
+    file_handler = logging.FileHandler("data/%s" % ('commit_%s.log' % formatted_time))
     file_handler.setLevel(logging.INFO)
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.INFO)
     logger.addHandler(file_handler)
     logger.addHandler(console_handler)
 
-    commit_data = defaultdict(lambda: defaultdict(int))
+    commit_data = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
 
     # If log already exists, read existing data
     if path.exists(json_log):
@@ -81,15 +104,17 @@ if __name__ == '__main__':
             for shard in existing_data[key].keys():
                 commit_data[key][shard] = existing_data[key][shard]
 
-    def collect_data(shard, q):
-        metadata = request(endpoint[shard], nodeMetadata())
-        block = request(endpoint[shard], latestBlock())
+    def collect_data(network, endpoint, q):
+        metadata = request(endpoint, nodeMetadata())
+        block = request(endpoint, latestBlock())
 
         if metadata != None and block != None:
             commit = metadata['version']
             height = int(block['blockNumber'])
-            logger.info('Commit: %s\tShard: %d\tBlock: %d' % (commit, shard, height))
-            q.put((commit, shard, height))
+            shard = str(block['shardID'])
+            timestamp = str(block['timestamp'])
+            logger.info('Network: %s\tCommit: %s\tShard: %s\tBlock: %d\tTimestamp: %s' % (network, commit, shard, height, timestamp))
+            q.put((network, commit, shard, height, timestamp))
 
     backup_counter = 0
     while True:
@@ -103,20 +128,21 @@ if __name__ == '__main__':
                 backup_counter += 1
             threads = []
             q = queue.Queue(maxsize = 0)
-            for x in range(len(endpoint)):
-                threads.append(Thread(target = collect_data, args = (x, q)))
+            for n in networks.keys():
+                for e in networks[n]:
+                    threads.append(Thread(target = collect_data, args = (n, e, q)))
             for t in threads:
                 t.start()
             for t in threads:
                 t.join()
             while not q.empty():
-                now = datetime.now().strftime(read_time_fmt)
-                v, s, h = q.get()
-                if not commit_data[v]:
-                    commit_data[v]['first'] = now
-                if h > commit_data[v][str(s)]:
-                    commit_data[v][str(s)] = h
-                    commit_data[v]['latest'] = now
+                n, v, s, h, t = q.get()
+                if not commit_data[n][v]:
+                    commit_data[n][v]['first'] = t
+                    commit_data[n][v]['first-block'] = h
+                if h > commit_data[n][v][str(s)]:
+                    commit_data[n][v]['latest'] = t
+                    commit_data[n][v][str(s)] = h
             with open(json_log, 'w') as f:
                 json.dump(commit_data, f, sort_keys = True, indent = 4)
             time.sleep(args.sleep)
